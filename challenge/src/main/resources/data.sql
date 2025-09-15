03f47f72c53ba4a202b36e6ee12c179301c7d1b9
mathiuSpv <mathius998@gmail.com>
2025-09-14 21:45:05 -0300

    chore: agregado dependencias & application.properties & .sql (Para generar datos de prueba)

---
+++ challenge/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Currency(codigo, name)
+VALUES ('ARS', 'Pesos Argentinos');
+
+INSERT INTO Currency(codigo, name)
+VALUES ('EUR', 'Euros');
+
+INSERT INTO Currency(codigo, name)
+VALUES ('USD', 'Dolares Americanos');

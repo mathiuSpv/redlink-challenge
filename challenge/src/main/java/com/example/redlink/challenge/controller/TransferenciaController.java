075daafabf09305d8b09fed2f779ab5daac02855
mathiuSpv <mathius998@gmail.com>
2025-09-15 22:27:04 -0300

    refactor: cambios de mejora de codigo y agregado los swaggers docs

--- challenge/src/main/java/com/example/redlink/challenge/controller/TransferenciaController.java
+++ challenge/src/main/java/com/example/redlink/challenge/controller/TransferenciaController.java
@@ -3,6 +3,9 @@ package com.example.redlink.challenge.controller;
 import com.example.redlink.challenge.persistence.entity.Transferencia;
 import com.example.redlink.challenge.service.DivisaServices;
 import com.example.redlink.challenge.service.TransferenciaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,18 +13,24 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/transferencia")
 @RequiredArgsConstructor
+@Tag(name = "Transferencias", description = "Operaciones para manejar transferencias bancarias")
 public class TransferenciaController {
 
     private final TransferenciaService transferenciaService;
     private final DivisaServices divisaServices;
 
+
     // Caso a
+    @Operation(summary = "Crear una transferencia")
+    @ApiResponse(responseCode = "200", description = "Transferencia exitosa")
+    @ApiResponse(responseCode = "400", description = "Moneda inválida")
     @PostMapping
     public ResponseEntity<Transferencia> crearTransferencia(@RequestBody Transferencia transferencia) {
         if (!divisaServices.monedaCorrecta(transferencia.getCurrency())) {
@@ -32,6 +41,10 @@ public class TransferenciaController {
     }
 
     // Caso b
+    @Operation(summary = "Consultar estado transferencia", description = "Consulta el estado de una transferencia por UUID. Permite conversión de moneda")
+    @ApiResponse(responseCode = "200", description = "Transferencia encontrada")
+    @ApiResponse(responseCode = "404", description = "Transferencia no encontrada")
+    @ApiResponse(responseCode = "500", description = "Moneda o Tasa invalida")
     @GetMapping("/{id}")
     public ResponseEntity<Transferencia> consultarEstado(
             @PathVariable UUID id,
@@ -52,6 +65,9 @@ public class TransferenciaController {
     }
 
     // Caso c
+    @Operation(summary = "Listar aprobadas", description = "Lista todas las transferencias aprobadas de un usuario")
+    @ApiResponse(responseCode = "200", description = "Transferencias encontradas")
+    @ApiResponse(responseCode = "404", description = "Usuario no existe")
     @GetMapping("/user/{userId}/aprobadas")
     public ResponseEntity<List<Transferencia>> listarAprobadas(
             @PathVariable String userId
@@ -66,6 +82,9 @@ public class TransferenciaController {
     }
 
     // Caso d
+    @Operation(summary = "Historial de transferencias", description = "Devuelve todo el historial de transacciones de un usuario")
+    @ApiResponse(responseCode = "200", description = "Historial encontrado")
+    @ApiResponse(responseCode = "404", description = "Usuario no existe")
     @GetMapping("/user/{userId}/historial")
     public ResponseEntity<List<Transferencia>> historial(
             @PathVariable String userId
@@ -81,16 +100,21 @@ public class TransferenciaController {
 
     private Transferencia aplicarConversionIfHaveParam(Transferencia t, String moneda, BigDecimal tasa) {
         // Si existen parametros de moneda y tasa entonces hace el cambio
-        if (moneda != null && tasa != null) {
-            if (tasa.compareTo(BigDecimal.ZERO) <= 0) {
-                throw new IllegalArgumentException("La tasa de conversión debe ser mayor a 0");
-            }
+        if (moneda == null && tasa == null)
+            return t;
 
-            if (divisaServices.monedaCorrecta(moneda)) {
-                t.setAmount(convertirMonto(t.getAmount(), tasa));
-                t.setCurrency(moneda);
-            }
-        }
+        assert moneda != null;
+        if (Objects.equals(moneda.toUpperCase(), t.getCurrency()))
+            return t; // Seria mejor volver un error? No sabria como encarar esto.
+
+        if (tasa.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("La tasa de conversión debe ser mayor a 0");
+
+        if (!divisaServices.monedaCorrecta(moneda.toUpperCase()))
+            throw new IllegalArgumentException("Tipo de moneda incorrecta");
+
+        t.setAmount(convertirMonto(t.getAmount(), tasa));
+        t.setCurrency(moneda.toUpperCase());
         return t;
     }
 
--- challenge/src/main/java/com/example/redlink/challenge/persistence/repository/DivisaRepository.java
+++ challenge/src/main/java/com/example/redlink/challenge/persistence/repository/DivisaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DivisaRepository extends JpaRepository<Divisa, String> {
+public interface DivisaRepository extends JpaRepository<Divisa, Integer> {
     /*
     * La idea de DivisaRepository es crear una validacion existsByCodigo,
     * esto tambien lo podriamos utilizar para tener el nombre completo de la divisa.
